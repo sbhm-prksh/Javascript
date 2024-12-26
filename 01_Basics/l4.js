31d5244517eb8cee4be7c9c2e3da1cd4e33b41f3
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2024-12-26 19:28:49 +0000

    Lecture 4: var let const

---
+++ 01_Basics/l4.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="shubham@google.com"
+var accountPassword="12345"
+accountCity="Jaipur" //will be var by nature
+
+// accountId=2 //not allowed
+// let accountEmail="shubham@uber.com" // let cant be redefined.
+accountEmail="sp@uber.com"
+accountPassword="54321"
+accountCity="Bengaluru"
+console.table([accountId,accountEmail, accountPassword, accountCity])
+
+/*
+this is a multi
+line comment
+*/
