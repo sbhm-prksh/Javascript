1db3106939bc880bbce2b1d47767901757fab59b
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2024-12-26 20:04:11 +0000

    L5: ecma and datatypes

---
+++ 01_Basics/l5.js
@@ -0,0 +1,19 @@
+"use strict"
+// alert(3+3) //will not work because we are using node.js and not browser.
+/*
+console.log(4+
+
+
+    3
+)
+*/ //bad practise.
+console.log(3+4) 
+console.log("Hello, I am working hard and think my dream will fulfill for sure.")
+
+let names="Shubham"
+let age=22
+let isLoggedId=false
+let balance=null
+let temp
+console.table([names, age, isLoggedId, temp, balance])
+console.table([typeof (names), typeof age, typeof isLoggedId, typeof temp, typeof balance])
