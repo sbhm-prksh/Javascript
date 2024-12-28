70cc2329c14a27439af2b4790063701be3badd50
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2024-12-28 12:04:30 +0000

    l5: datatype conversion

---
+++ 01_Basics/l6.js
@@ -0,0 +1,24 @@
+// Number conversion
+let score = "33"
+console.log(typeof score)
+let valueInNumber=Number(score) // we will have successfull conversion here.
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+// // //////////////////////////////////////////
+let score2="shreya"
+console.log(typeof score2)
+let valueInNumber2=Number(score2) // unsuccessfull conversion. we will be getting a NaN which is also a number.
+console.log(valueInNumber2)
+console.log(typeof valueInNumber2)
+// //////////////////////////////////////////
+// Boolean Conversion
+let isLoggedIn=undefined
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// //////////////////////////////////////////
+// String conversion
+let someNumber="shreya"
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
