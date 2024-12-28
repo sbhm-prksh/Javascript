598bcec662fe50aa9af4928844a93cdd6927482f
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2024-12-28 13:59:12 +0000

    l9: datatypes summary

---
+++ 01_Basics/l9.js
@@ -0,0 +1,27 @@
+// //  Dynamically typed language.
+//  let num=12
+//  num="Shubham"
+//  console.log(num)
+
+//  Symbols
+let num1=Symbol(123)
+let num2=Symbol(123)
+console.log(num1===num2)
+console.log(typeof num1)
+// // bigint
+// const bigNumber=2347239847928374902384n
+// console.log(bigNumber)
+// console.log(typeof bigNumber)
+// // Non Primitive Datatypes
+// const hero=['elon', 'sundar', 'travis kalanick', 'sam altman']
+// let myObj={
+//     name:"shubham",
+//     age:22,
+// }
+// const myFunction= function(){
+//     console.log("Hello World")
+// }
+// console.log(typeof hero)
+// console.log(typeof myObj)
+// console.log(typeof myFunction)
+
