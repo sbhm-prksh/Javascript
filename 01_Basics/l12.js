f1fc84b4ac4d74d6eb9f62be90cf829574f33bc4
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-01-01 06:26:55 +0000

    l12:numbers and maths

---
+++ 01_Basics/l12.js
@@ -0,0 +1,20 @@
+// Numbers
+// const score=400
+// console.log(score)
+// console.log(typeof score)
+
+// const balance=new Number(5000)
+// console.log(balance)
+// console.log(typeof balance)
+
+// console.log(balance.toString())
+// console.log(typeof (balance.toString()))
+// console.log(balance.toFixed())
+// console.log(balance.toPrecision(1))
+// const myAMount=10000000000
+// console.log(myAMount.toLocaleString('en-IN'))
+// console.log(Number.MAX_SAFE_INTEGER)
+
+// MATHS
+console.log(Math.floor(41.99))
+// console.log(Number.abs(-4))
