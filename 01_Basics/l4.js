656730112408654b4dbcb7ff47b20b1d02a06940
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-03-08 14:06:24 +0000

    Revised 1-5

--- 01_Basics/l4.js
+++ 01_Basics/l4.js
@@ -3,12 +3,12 @@ let accountEmail="shubham@google.com"
 var accountPassword="12345"
 accountCity="Jaipur" //will be var by nature
 
-// accountId=2 //not allowed
+// accountId=2 //not allowed because it is a const
 // let accountEmail="shubham@uber.com" // let cant be redefined.
 accountEmail="sp@uber.com"
 accountPassword="54321"
-var accountCity="Bengaluru-2" //var can be redefined.
-// accountCity="Bengaluru"
+var accountCity="Bengaluru" //var can be redefined.
+// accountCity="Hydrabad"
 var accountSettings //undefined
 console.table([accountId,accountEmail, accountPassword, accountCity, accountSettings])
 
