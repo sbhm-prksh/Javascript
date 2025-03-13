a961763eb2e47867483cd6c8397d8c07d9b27d36
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-03-13 12:40:20 +0000

    Date and time

--- 01_Basics/l13.js
+++ 01_Basics/l13.js
@@ -1,19 +1,26 @@
 // DATE
 let myDate = new Date()
-console.log(myDate)
-console.log(myDate.getMonth())
-console.log(typeof myDate) //ugly
-myDate.setFullYear(2972)
-console.log(myDate)
-console.log(typeof myDate)
+console.log(typeof myDate) //object
+console.log(myDate) //ugly
+// Ways of printing dates in good manner. Use 'en-IN' inside toLocale to have in d/m/y formate.
 console.log(myDate.toString())
 console.log(myDate.toLocaleString())
-let myNewDate=new Date(2025, 0,23)
-console.log(myNewDate.toDateString())
-// let myNewDate = new Date(2023,2,12)
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getMonth())
+
+let myNewDate=new Date(0) // Since how many ms from UNIX epoch
+console.log(myNewDate);
+
+// getting and setting the date
+myNewDate = new Date(2023,2,12)
+myDate.setFullYear(2972)
 console.log(myNewDate.getFullYear())
+
+
 
 // TIME
 let myTime=Date.now()
+// let myTime= new Date.now()  // this is wrong
 console.log(typeof Date.now())
 console.log(myTime)
