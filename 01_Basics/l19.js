6652ccb5ca7ac522db5f5aa42fce3b41aeed4102
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-03-20 18:17:14 +0000

    Functions

---
+++ 01_Basics/l19.js
@@ -0,0 +1,28 @@
+// FUNCTIONS
+
+// function sayMyName(){
+//     console.log("S");
+//     console.log("H");
+//     console.log("U");
+//     console.log("B");
+//     console.log("H");
+//     console.log("A");
+//     console.log("M");
+// };
+// sayMyName; //This is just a reference
+// sayMyName(); // This is executing that function
+
+// FUNCTION WITH PARAMETERS; 
+function addTwoNumbers(number1, number2)
+{
+    console.log(number1+number2);
+}
+// addTwoNumbers();
+// addTwoNumbers(1);
+// addTwoNumbers("1");
+// addTwoNumbers("abcd");
+// addTwoNumbers(1, null);
+// addTwoNumbers("1", null);
+let result=addTwoNumbers(3,5); //result will be undefined
+// console.log(result)
+
