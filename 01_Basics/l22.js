378d9bb120a11fb0a7e789ac0359ec2b540be237
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-03-30 12:27:16 +0000

    Hoisting: L22

---
+++ 01_Basics/l22.js
@@ -0,0 +1,28 @@
+// BLOCK SCOPE IN A FUNCTION
+function one()
+{
+    let userName="Shubham";
+    function two()
+    {
+        let userAge=23;
+        console.log(userAge);
+        console.log(userName);
+    }
+    // console.log(userAge) //Out of its scope
+    two();
+}
+// console.log(userName); //Out of its scope
+one()
+
+
+// HOISTING BRIEF
+console.log(addOne(5)) // WORKS
+function addOne(num)
+{
+    return num+1;
+}
+console.log(addTwo(5)) //DOESNT WORKS
+let addTwo=function(num)
+{
+    return num+1;
+}
