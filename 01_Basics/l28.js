041165b36493fdce66247aaa0d5ca1d03973ee91
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-04-02 12:47:13 +0000

    while loop

---
+++ 01_Basics/l28.js
@@ -0,0 +1,22 @@
+// WHILE LOOP
+let index=0;
+while(index<=10)
+{
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+let myArr=["Batman", "IronMan", "Goole"];
+let arr=0;
+while(arr<myArr.length)
+{
+    console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+
+// DO WHILE LOOP
+let score=11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
