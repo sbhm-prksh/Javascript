bc02786f03f9ff74a45b6b5a49c7eec14bc1aa43
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-04-15 07:18:11 +0000

    Project1: Edits

--- 02_Projects/Project_1/script.js
+++ 02_Projects/Project_1/script.js
@@ -1,12 +1,13 @@
 let buttons=document.querySelectorAll('.colorChanger')
 let body=document.querySelector('body')
 // BELOW FOREACH WILL SET EVENTLISTENER TO EACH BUTTONS.
-// ALTERNATE WAY IS TO SET EVENTLISTENER TO EACH BUTTONS INDIVISUALLY
+// ALTERNATE WAY IS TO SET EVENTLISTENER TO EACH BUTTONS INDIVISUALLY. BUT VIOLETS DRY
 buttons.forEach((button)=>{
     button.addEventListener('click', (evnt)=>{
         // console.log(evnt) // Whole event object which contains all necessary information about the even
         // console.log(evnt.target) // It gives us the html that has triggered the event.
         // console.log(evnt.target.id) //Now you can apply id, className or whatever on that html
+        console.log(evnt.target.style.backgroundColor)
         body.style.backgroundColor=evnt.target.id;
     })
 })
