02b2396ed20d2b5bd073f1d0fcef641d0fdc4007
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-04-15 05:36:39 +0000

    DOM edit

--- 01_Basics/l32.js
+++ 01_Basics/l32.js
@@ -1,3 +1,4 @@
 // UNDERSTANDING DOM!
 let a=document.getElementById('title')
-a.innerHTML="<i>shubham</i>"
+a.innerHTML="<i>shubham</i>" //it will be treated as html and shubham will get italic
+a.innerText="<i>shubham</i>" //it will be treated as raw text
