45f589a1a1af688cf7094ef4e6de92c108b5d1bf
Shubham Prakash <114825292+sbhm-prksh@users.noreply.github.com>
2025-04-17 05:46:11 +0000

    Project4: Comments Add

--- 02_Projects/Project_4/script.js
+++ 02_Projects/Project_4/script.js
@@ -1,70 +1,82 @@
+// INTIAL VALUES
 let targetNum=Math.ceil(1+99*Math.random())
 let attemptRemaining=10;
 let guessedNumber=[];
+
+// SETTING DOM TO NEW VALUES
 let guessedNumberString=guessedNumber.join();
 document.getElementById('guessArray').innerHTML=guessedNumberString
 document.getElementById('attemptCount').innerHTML=attemptRemaining
-console.log(targetNum)
-console.log(targetNum)
-
 
 let form=document.querySelector('form')
 form.addEventListener('submit',(evnt)=>{
     evnt.preventDefault();
     let enteredNum=Number(document.getElementById("number").value);
+
+    // CHECKING IF NO INPUT IS THERE
     if(document.getElementById("number").value=='')
     {
         alert("Kindly enter a number")
     }
+
+    // CHECKING WHETHER NUMBER IS IN RANGE OR NOT
     else if(enteredNum>100 || enteredNum<0)
     {
         alert("Kindly enter number in 1 to 100 range")
 
     }
+    // IF EVERYTHING IS OKAY
     else
     {
+        // SETTING DOM TO NEW VALUES
         attemptRemaining--;
         guessedNumber.push(enteredNum)
         let guessedNumberString=guessedNumber.join();
         document.getElementById('guessArray').innerHTML=guessedNumberString
         document.getElementById('attemptCount').innerHTML=attemptRemaining
 
+        // IF USER GOT THE CORRECT NUMBER
         if(enteredNum==targetNum)
         {
             document.querySelector('button[type="reset"]').innerHTML="Restart Game"
             document.getElementById('won').innerHTML=`Congratulation You Won! You entered the correct number i.e ${targetNum}.\n Kindly restart the game.`
             document.querySelector('button[type="submit"]').disabled=true;
         }
+
+        // IF WRONG GUESS
         else
         {
             document.getElementById('won').innerHTML=`Oh No,Wrong Guess! Try again`
         }
+
+        // IF NO FURTHER ATTEMPT IS REMAINING
         if(attemptRemaining==0)
         {
             document.querySelector('button[type="reset"]').innerHTML="Restart Game"
             document.getElementById('won').innerHTML=`You lost! The correct number was ${targetNum}.`
-            // document.querySelector('button[type="submit"]').setAttribute('disabled', 'true');
             document.querySelector('button[type="submit"]').disabled=true;
 
         }
-        document.getElementById("number").value = '';
-
+        document.getElementById("number").value = ''; // Making form empty again for next attempt
     }
 })
+
+// RESTART OR RESET
 form.addEventListener('reset', (evnt)=>{
     evnt.preventDefault();
+    // GETTING NEW VALUES
     targetNum=Math.ceil(1+99*Math.random())
-    console.log(targetNum)
     attemptRemaining=10;
     guessedNumber=[];
+    guessedNumberString=guessedNumber.join()
+
+    // RE ADJUSTING DOM TO INITIAL STATE
     document.getElementById('guessArray').innerHTML=guessedNumberString
     document.getElementById('attemptCount').innerHTML=attemptRemaining
-    document.getElementById('won').innerHTML=``
-    document.getElementById("number").value = '';
-    console.log(targetNum)
-    document.querySelector('button[type="submit"]').disabled=false;
     document.querySelector('button[type="reset"]').innerHTML="Reset Game"
+    document.getElementById('won').innerHTML=``
 
-
-
+    // ENABLING SUBMIT BUTTON WITH NULL VALUES
+    document.querySelector('button[type="submit"]').disabled=false;
+    document.getElementById("number").value = '';
 })
---
+++ 02_Projects/Project_4/script2.js
@@ -0,0 +1,29 @@
+const submit=document.getElementById('subt')
+const userInput=document.getElementById('guessField')
+const guessSlot=document.querySelector('.guesses')
+const remaining=document.querySelector('.lastResult')
+const startOver=document.querySelector('.resultParas')
+const p=document.createElement('p')
+
+let randomNumber=(Math.round(1+Math.random()*99))
+let prevGuess=[];
+let numGuess=1;
+let playGame=true;
+
+function validateGuess(guess)
+{
+
+}
+
+function checkGuess(guess)
+{
+
+}
+function displayGuess(guess)
+{
+    
+}
+function displayMessage(message)
+{
+
+}
